8674034eac5c440501ef82cd8a3e48abcc888a15
1dlvb <matushkin-2017@mail.ru>
2024-01-19 17:33:06 +0500

    Bakery Service have been added.

--- server/src/main/java/ru/midas/server/service/BakeryService.java
+++ server/src/main/java/ru/midas/server/service/BakeryService.java
@@ -1,4 +1,15 @@
-package ru.midas.server.services;
+package ru.midas.server.service;
 
-public class BakeryService {
+import ru.midas.server.model.Bakery;
+
+import java.util.List;
+
+public interface BakeryService {
+    List<Bakery> fetchBakeryList();
+    Bakery saveBakery(Bakery bakery);
+
+    Bakery findBakeryById(Long id);
+
+    Bakery updateBakery(Bakery bakery);
+    void deleteBakery(Long id);
 }
