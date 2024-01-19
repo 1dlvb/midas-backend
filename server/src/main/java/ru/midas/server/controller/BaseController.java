c6a981376a3f94a00f7c68de867461f5b19b4c96
1dlvb <matushkin-2017@mail.ru>
2024-01-19 17:31:03 +0500

    controllers have been added.

--- server/src/main/java/ru/midas/server/controller/BaseController.java
+++ server/src/main/java/ru/midas/server/controller/BaseController.java
@@ -1,12 +1,8 @@
-package ru.midas.server.controllers;
+package ru.midas.server.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.midas.server.models.Bakery;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/midas")
