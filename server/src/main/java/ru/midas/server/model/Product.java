94464e0fbbc067d1a22c8f7075109c24acad34e9
1dlvb <matushkin-2017@mail.ru>
2024-01-20 22:44:39 +0500

    Product logic have been changed. Product is only one class for different types of products. We have moved away from the system of inheritance and categorization

--- server/src/main/java/ru/midas/server/model/Product.java
+++ server/src/main/java/ru/midas/server/model/Product.java
@@ -1,2 +1,44 @@
-package ru.midas.server.model;public class Product {
+package ru.midas.server.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @Column(unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(cascade=CascadeType.MERGE)
+    @JoinColumn(name = "category_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Category category;
+
+    @Column(nullable = false)
+    private String Name;
+
+    @Column(nullable = false)
+    private String Description;
+
+    @Column(nullable = false)
+    private String ImageUrl;
+
+    @Column(nullable = false)
+    private Integer Quantity;
+
+    @Column(nullable = false)
+    private Integer Price;
+
+    @Column(nullable = false)
+    private Integer Weight;
+
 }
+
