ce3c5c561cdabff233a1d5a8c23bcba5a400e726
1dlvb <matushkin-2017@mail.ru>
2024-02-02 20:30:03 +0500

    findProductsByCategoryId has been cut out.

--- src/main/java/ru/midas/server/repository/ProductRepository.java
+++ src/main/java/ru/midas/server/repository/ProductRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.midas.server.model.Product;
 
-import java.util.List;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findProductById(Long id);
