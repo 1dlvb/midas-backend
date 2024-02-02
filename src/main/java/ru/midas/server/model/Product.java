8f5ea4713db2a4b4dc9ba2fe384c41fc0c766ff0
1dlvb <matushkin-2017@mail.ru>
2024-02-02 20:30:03 +0500

    findProductsByCategoryId has been cut out.

--- src/main/java/ru/midas/server/model/Product.java
+++ src/main/java/ru/midas/server/model/Product.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Data
