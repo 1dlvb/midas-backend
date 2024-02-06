f4f3e2b02037215f8bb095f9de8ff7a74a589ffc
1dlvb <matushkin-2017@mail.ru>
2024-02-06 17:55:28 +0500

    user login system have been added.

--- src/main/java/ru/midas/server/api/model/LoginBody.java
+++ src/main/java/ru/midas/server/api/model/LoginBody.java
@@ -3,8 +3,10 @@ package ru.midas.server.api.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class LoginBody {
 
     @NotNull
